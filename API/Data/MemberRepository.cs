56c13f62677e39a0f2e327673edbd20605ef145e
manishrathod01 <manishrathod384@gmail.com>
2025-07-10 23:00:36 +0530

    end of section 10

--- API/Data/MemberRepository.cs
+++ API/Data/MemberRepository.cs
@@ -35,4 +35,10 @@ public class MemberRepository(AppDbContext context) : IMemberRepository
     {
         context.Entry(member).State = EntityState.Modified;
     }
+
+    public async Task<Member?> GetMemberForUpdate(string id)
+    {
+        return await context.Members.Include(x => x.User)
+        .SingleOrDefaultAsync(x => x.Id == id);
+    }
 }
